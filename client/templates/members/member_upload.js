1a065bb61f0fd57c886093835b94963a8ae8715a
David Mulligan <davidmulligan@btopenworld.com>
2015-02-27 17:56:51 +0000

    Reworked CSV upload to use PapaParse

--- client/templates/members/member_upload.js
+++ client/templates/members/member_upload.js
@@ -1,26 +1,46 @@
+Template.memberUpload.created = function() {
+    this.importResult = new ReactiveVar();
+};
+
+Template.memberUpload.helpers({
+    importResult: function() {
+        return Template.instance().importResult.get();
+    }
+});
+
 Template.memberUpload.events({
-    'change .uploadFile' : function(evt, tmpl) {
-        FS.Utility.eachFile(event, function(file) {
-           Uploads.insert(new FS.File(file), function(error, fileObj) {
-               if (!error) {
-
-                   // Upload the members
-                   Meteor.call('uploadFile', fileObj._id, file.name, function(err, data) {
-                       if (!err) {
-
-                           // Display confirmation message
-                           FlashMessages.sendSuccess("Members successfully uploaded.");
-                       }
-                   });
-
-                   Router.go('memberList');
-               } else {
-                   FlashMessages.sendError("Failed to upload file : " + file.name);
-               }
-           })
-       })
+    'click .upload': function(event, template) {
+        var fileInput = template.find('input[type=file]');
+        if (fileInput.files.length === 0) {
+            bootbox.alert("Please select a file.");
+        } else {
+            Papa.parse(fileInput.files[0], {
+                complete: function(results) {
+                    template.importResult.set(results.data.map(function(item, index) {
+                        item.id = index;
+                        return item;
+                    }));
+                }
+            });
+        }
     },
-    'click .cancel': function(e, tpl) {
+    'click .import': function(e, template) {
+        var selected = template.findAll("input[type=checkbox]:checked");
+        var memberArray = _.map(selected, function(item) {
+            return Template.instance().importResult.get()[item.defaultValue];
+        });
+        if (memberArray.length > 0) {
+            Meteor.call('insertMembers', memberArray, function(error) {
+                if (error) {
+                    FlashMessages.sendError(error.reason);
+                } else {
+                    FlashMessages.sendSuccess("Successfully imported " + memberArray.length + " members.");
+                    Router.go('memberList');
+                }
+            });
+        }
+    },
+    'click .cancel': function() {
         Router.go('memberList');
     }
 });
