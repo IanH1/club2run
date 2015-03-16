dbfdf3f9fb214fa34d3de26fdbdae99169d760b4
David Mulligan <davidmulligan@btopenworld.com>
2015-03-16 00:25:43 +0000

    Minor improvements

--- client/helpers/lib/autoform_config.js
+++ client/helpers/lib/autoform_config.js
@@ -1,5 +1,6 @@
 AutoForm.addHooks(null, {
     onError: function(operation, error) {
+        console.log("An error occured submitting form :" + operation + ", error : " + error);
         FlashMessages.sendError(error.reason);
     }
 });
